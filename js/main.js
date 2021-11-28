347edb85defeda2cbfd00149788ba4475801c18b
Bikiscripter <Sumikalita99999999@gmail.com>
2021-11-28 07:55:47 +0530

    b

---
+++ js/main.js
@@ -0,0 +1,72 @@
+function yt() {
+  let put = document.getElementById('put')
+  put.innerHTML = " "
+  var img = document.createElement('img');
+  img.src = './img/yt1.jpg';
+  img.classList.add('yg')
+  document.getElementById('put').appendChild(img);
+  // down.innerHTML = "Image Element Added.";
+  var ig = document.createElement('img');
+  ig.src = './img/yt2.jpg';
+  ig.classList.add('yg')
+  document.getElementById('put').appendChild(ig);
+  // down.innerHTML = "Image Element Added.";
+  var g = document.createElement('img');
+  g.src =
+  './img/yt3.jpg';
+  g.classList.add('yg')
+  document.getElementById('put').appendChild(g);
+  // down.innerHTML = "Image Element Added.";
+  var h = document.createElement('img');
+  h.src = './img/yt4.jpg';
+  h.classList.add('yg')
+  document.getElementById('put').appendChild(h);
+  // down.innerHTML = "Image Element Added.";
+  var p = document.createElement('img');
+  p.src = './img/yt5.jpg';
+  p.classList.add('yg')
+  document.getElementById('put').appendChild(p);
+  // down.innerHTML = "Image Element Added.";
+  let k = document.getElementById('link')
+  k.innerHTML = " "
+  var img = document.createElement('div');
+  img.innerHTML = "YOUTUBE LINK => https://youtube.com/c/BikiScripter"
+  img.classList.add('linkk')
+  document.getElementById('link').appendChild(img);
+  
+}
+function tele() {
+  let jk = document.getElementById('put')
+  jk.innerHTML = " "
+  var img = document.createElement('img');
+  img.src = './img/tele1.jpg';
+  img.classList.add('yg')
+  document.getElementById('put').appendChild(img);
+  // down.innerHTML = "Image Element Added.";
+  var h = document.createElement('img');
+  h.src = './img/tele2.jpg';
+  h.classList.add('yg')
+  document.getElementById('put').appendChild(h);
+  // down.innerHTML = "Image Element Added.";
+  let kp = document.getElementById('link')
+  kp.innerHTML = " "
+  var imjg = document.createElement('div');
+  imjg.innerHTML = "TELEGRAM LINK => https://t.me/Biki_Scripter"
+  imjg.classList.add('linkk')
+  document.getElementById('link').appendChild(imjg);
+}
+function Whatsapp() {
+  let jk = document.getElementById('put')
+  jk.innerHTML = " "
+  var pp = document.createElement('img');
+  pp.src ='./img/wa1.jpg';
+  pp.classList.add('yg')
+  document.getElementById('put').appendChild(pp);
+  // down.innerHTML = "Image Element Added.";
+let kpp = document.getElementById('link')
+  kpp.innerHTML = " "
+  var imjpg = document.createElement('div');
+  imjpg.innerHTML = "WHATSAPP LINK => https://wa.me/qr/K5HIGHXHB2JXE1"
+  imjpg.classList.add('linkk')
+  document.getElementById('link').appendChild(imjpg);
+}
